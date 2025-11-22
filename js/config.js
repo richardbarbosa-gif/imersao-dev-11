f19273a44a71412ca3dd3b1fb3e4b7a31e9bf3bf
richardbarbosa-gif <richardbarbosa28@gmail.com>
2025-11-22 18:13:42 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
-// Este arquivo guarda sua chave. NÃO suba ele para o GitHub.
+
 const CONFIG = {
     API_KEY: "considere colocar uma chave "API KEY" sua e divirta se com a sabedoria do oraculo"
 
 };
+
